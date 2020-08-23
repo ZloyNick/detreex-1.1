89780002fd3a51bfa7199f206e0ab8e18277fda5
ZloyNick <ravil.parkour@gmail.com>
2020-08-24 00:31:45 +0500

    API - pocketmine\Player some extends. Player - Initialize

--- plugins/GameBase/src/GameBase/player/Player.php
+++ plugins/GameBase/src/GameBase/player/Player.php
@@ -1,10 +1,56 @@
 <?php
 
+declare(strict_types=1);
 
 namespace GameBase\player;
 
+use pocketmine\level\Position;
+use pocketmine\Player as _Player;
 
 class Player
 {
+    public function __construct(_Player $player)
+    {
+        static::$player = $player;
+        static::init();
+    }
+
+    public function getPlayer() : _Player
+    {
+        return static::$player;
+    }
+
+    private static function init() : void
+    {
+
+    }
+
+    /**
+     * @var _Player
+     */
+    private static
+        $player;
+
+    /** Implementations */
+
+    public function getPosition() : Position
+    {
+        return static::$player->getPosition();
+    }
+
+    public function getName() : string
+    {
+        return static::$player->getName();
+    }
+
+    public function getDisplayName() : string
+    {
+        return static::$player->getDisplayName();
+    }
+
+    public function setDisplayName(string $newName) : void
+    {
+        static::$player->setDisplayName($newName);
+    }
 
 }
